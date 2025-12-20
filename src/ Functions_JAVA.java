63ac3584f54c7916916cf39567b09b7ca0ae70de
fatimaaziz27 <fatimaaziz250907@gmail.com>
2025-12-20 16:02:15 +0000

    mnb

--- src/ Functions_JAVA.java
+++ src/ Functions_JAVA.java
@@ -1,38 +1,59 @@
 import java.util.*;
-class BASIC_JAVA {
 
-// Java program to check if a number is prime or not:
-// CODE:
+// // Java program to check if a number is prime or not:
+// // CODE:
 
-      public static String prime(int num) {
-        for(int i=2;i<=num;i++){
-            if(num%i==0){
-                return num+ "Not a Prime number";
-            }
-        }
-        return num+ "is a Prime number";
-    } 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-        System.out.println(prime(number));
-}
+//       public static String prime(int num) {
+//         for(int i=2;i<=num;i++){
+//             if(num%i==0){
+//                 return num+ "Not a Prime number";
+//             }
+//         }
+//         return num+ "is a Prime number";
+//     } 
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int number = sc.nextInt();
+//         System.out.println(prime(number));
+// }
 
-    Scanner number = new Scanner(System.in);
-        System.out.println("enter any number:");
-        int num = number.nextInt();
-        if (num>0){
-            if (num%2 == 0){
-                System.out.print("the entered num is positive & even");
-            }
-            else{
-                System.out.println("enter number is positive & odd");
-            }
-        }
-        else{
-            System.out.println("the entered num is negative");
-        }
+//     Scanner number = new Scanner(System.in);
+//         System.out.println("enter any number:");
+//         int num = number.nextInt();
+//         if (num>0){
+//             if (num%2 == 0){
+//                 System.out.print("the entered num is positive & even");
+//             }
+//             else{
+//                 System.out.println("enter number is positive & odd");
+//             }
+//         }
+//         else{
+//             System.out.println("the entered num is negative");
+//         }
 
+class StudentData {
+    String name;
+    int rollNo;
+    int marks;
+
+    void displayDetails() {
+        System.out.println(
+            "Name: " + name +
+            "\nRoll No: " + rollNo +
+            "\nMarks: " + marks
+        );
     }
 }
+
+public class Function_JAVA {
+    public static void main(String[] args) {
+        StudentData student1 = new StudentData();
+        student1.name = "YUSRA";
+        student1.rollNo = 20;
+        student1.marks = 95;
+        student1.displayDetails();
+    }
+}
+
