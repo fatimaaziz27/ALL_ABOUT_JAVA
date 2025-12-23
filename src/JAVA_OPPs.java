a7a2084e2e5e1bb2fa104c198f6b5967da104f99
fatimaaziz27 <fatimaaziz250907@gmail.com>
2025-12-23 23:50:19 +0500

    fixo1

--- src/JAVA_OPPs.java
+++ src/JAVA_OPPs.java
@@ -7,6 +7,8 @@ class JAVA_OOPs {
 //     procedural programming
 //     object oriented programming (OOP)
 
+//          Introduction to Object-Oriented Programming in Java
+
 // What is OOP?
 //     OOP stands for Object-Oriented Programming System (or Object-Oriented Programming).
 //     It’s a way of writing programs where you organize code around objects instead of just functions and logic.
@@ -28,6 +30,17 @@ class JAVA_OOPs {
 // Polymorphism        Multiple forms
 // Abstraction         Hide details
 
+
+//              Key Concepts of OOP:
+
+//  1. Object: An instance of a class. It contains data and
+//  methods defined by the class.
+
+//  2. Class: A blueprint for creating objects. It defines the
+//  properties (attributes) and behaviors (methods) that the
+//  objects created from the class will have.
+
+
 // PILLARS OF OOP:
 
 //     Encapsulation:
@@ -48,9 +61,24 @@ class JAVA_OOPs {
 //     It helps in reducing complexity and increases efficiency.
 
 
+//                  Benefits of OOP:
 
+//  1. Code Reusability: Through inheritance, existing code can be reused in new
+//   classes, reducing redundancy.
 
-//          Introduction to Object-Oriented Programming in Java
+//  2. Modularity: Code is organized into classes and objects, making it easier to
+//   manage and maintain.
+
+//  3. Flexibility: Polymorphism allows for flexible and dynamic code that can
+//   adapt to different situations.
+
+//  4. Easier Maintenance: Encapsulation helps in protecting data and ensures that
+//   objects can only be modified through well-defined methods, making maintenance
+//   easier.
+
+//  5. Real-World Modeling: OOP allows for better modeling of real-world entities
+//   and relationships, making it easier to understand and design complex systems.
+
 
     public static void main(String[] args) {
 
@@ -87,66 +115,6 @@ class car{
         System.out.println("Car Started");}
 
 }
-
-
-
-
-
-
-
-
-//Key Concepts of OOP:
-
-//1. Object: An instance of a class. It contains data and
-// methods defined by the class.
-
-//2. Class: A blueprint for creating objects. It defines the
-//  properties (attributes) and behaviors (methods) that the
-//  objects created from the class will have.
-
-
-
-
-
-
-
-// 3. Encapsulation: The bundling of data (attributes) and methods
-// (functions) that operate on that data into a single unit (class).
-// It restricts direct access to some of the object's components,
-// which helps prevent unintended interference and misuse of the data.
-
-// 4. Inheritance: A mechanism where a new class (subclass/child class)
-// can inherit properties and behaviors from an existing class
-// (superclass/parent class). This promotes code reusability and
-// establishes a hierarchical relationship between classes.
-
-// 5. Polymorphism: The ability of different classes to be treated as
-//  instances of the same class through a common interface. It allows
-// methods to do different things based on the object that it is
-// acting upon, even though they share the same name.
-
-// 6. Abstraction: The concept of hiding the complex implementation
-// details and exposing only the essential features of an object.
-// It helps in reducing complexity and increases efficiency.
-
-//Benefits of OOP:
-//1. Code Reusability: Through inheritance, existing code can be reused in new
-//   classes, reducing redundancy.
-//2. Modularity: Code is organized into classes and objects, making it easier to
-//   manage and maintain.
-//3. Flexibility: Polymorphism allows for flexible and dynamic code that can
-//   adapt to different situations.
-//4. Easier Maintenance: Encapsulation helps in protecting data and ensures that
-//   objects can only be modified through well-defined methods, making maintenance
-//   easier.
-//5. Real-World Modeling: OOP allows for better modeling of real-world entities
-//   and relationships, making it easier to understand and design complex systems.
-
-
-
-
-
-
 
 
 
