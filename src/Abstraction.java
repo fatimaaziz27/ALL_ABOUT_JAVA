05698ee71386789a226d96aebde874ab9b448920
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-02 20:48:44 +0500

    fixo

--- src/Abstraction.java
+++ src/Abstraction.java
@@ -1,8 +1,13 @@
-// Method Overloading:
-// Same method name, different parameters, same class.
+//                      Abstraction in Java:
 
-// Method Overriding:
-// Same method name, same parameters, child class changes parent method.
+
+// Abstraction is an OOP concept that focuses on what an object does, not how it does it.
+// It hides unnecessary details and shows only the important features to the user.
+// Abstract methods cannot be accessed without inheritance (parent and child classes).
+// Polymorphism is required, where the child class overrides the abstract method.
+// If inheritance or overriding is used incorrectly, the code will not give the expected output or the abstract method cannot be accessed.
+// Abstraction is used to hide data and expose only essential information.
+
 
 import java.util.*;
 class Abstraction {
