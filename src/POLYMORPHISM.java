2ed0711aa7a28b7c78a6bf5000c87cfef44964d5
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-10 22:20:18 +0500

    fio

--- src/POLYMORPHISM.java
+++ src/POLYMORPHISM.java
@@ -4,6 +4,22 @@ public class POLYMORPHISM {
 //1. Method Overloading
 //2. Method Overriding
 
+// class Polymorphism {
+//     public static void main(String[] args) {
+//         MathOperations mathOps = new MathOperations();
+//////
+//////         // Using method overloading
+//         System.out.println("Sum of 2 and 3: " + mathOps.add(2, 3));
+//         System.out.println("Sum of 2, 3 and 4: " + mathOps.add(2, 3,4));
+//         System.out.println("Sum of 2 and 3.5: " + mathOps.add(2, 3.5));
+//         System.out.println("Sum of 2 and 3.5: " + mathOps.add(2.5, 3.5));
+////     Animal5 dog2 = new Animal5();
+////     dog2.sound();
+////     Dog5 dog1 = new Dog5();
+////     dog1.sound();
+//     }
+// }
+
 
     
 }
