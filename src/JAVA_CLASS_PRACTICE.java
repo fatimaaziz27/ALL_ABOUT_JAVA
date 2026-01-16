8a8e1ed08bd458123f6edbbb3bfff51db5b15311
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-16 11:50:23 +0500

    fixi

--- src/JAVA_CLASS_PRACTICE.java
+++ src/JAVA_CLASS_PRACTICE.java
@@ -387,5 +387,35 @@ class car{
 //}
 
 
+// 6. Student Database Class
+// Define a class `StudentDatabase` that holds a list of students. Each student is represented as a dictionary
+// with their `name` and `age`. Provide methods to add a student, remove a student, and display all students.
+
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        student_database s1 = new student_database("me",18);
+        ArrayList<String> l1 = new ArrayList<>();
+    }
+}
+class student_database{
+    String name;
+    Integer age;
+    student_database(String name , Integer age){
+        this.name = name;
+        this.age= age;
+    }
+    void add(){
+        name.add("me");
+        name.add("me");
+    }
+    void remove(){
+        name.remove("me");
+    }
+    void display(){
+        System.out.println("data " + this.name);
+    }
+}
+
 
 
