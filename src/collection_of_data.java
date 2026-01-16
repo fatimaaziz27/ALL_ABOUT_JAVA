1260707e1808bc2c89669da70cbeae632c0d04be
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-16 11:49:51 +0500

    fixo

---
+++ src/collection_of_data.java
@@ -0,0 +1,28 @@
+// 6. Student Database Class
+// Define a class `StudentDatabase` that holds a list of students. Each student is represented as a dictionary
+// with their `name` and `age`. Provide methods to add a student, remove a student, and display all students.
+
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        student_database s1 = new student_database( );
+
+    }
+}
+class student_database{
+    String name;
+    Integer age;
+    student_database(String name , Integer age){
+        this.name = name;
+        this.age= age;
+    }
+    void add(){
+
+    }
+    void remove(){
+
+    }
+    void display(){
+
+    }
+}
