f5d68f6420e1dba4e3748926cefbb90d66c634c1
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-30 20:13:03 +0500

    fixi

--- src/collection_of_data.java
+++ src/collection_of_data.java
@@ -1,29 +1,72 @@
 // 6. Student Database Class
 // Define a class `StudentDatabase` that holds a list of students. Each student is represented as a dictionary
 // with their `name` and `age`. Provide methods to add a student, remove a student, and display all students.
-/*
+
+
 import java.util.*;
 class main{
     public static void main(String[] args){
-      //  student_database s1 = new student_database( );
+        student
 
     }
 }
 class student_database{
     String name;
     Integer age;
+
+    HashMap<String,Integer> h1 = new HashMap<>();
+
     student_database(String name , Integer age){
         this.name = name;
         this.age= age;
     }
     void add(){
-
+        h1.put(this.name,this.age);
     }
     void remove(){
-
+        h1.remove(this.name);
     }
     void display(){
-
+        System.out.println(h1);
     }
 }
-*/
+
+
+//import java.util.*;
+//class hashmaporset{
+//    public static void main(String[]args){
+//        // HASHMAP
+//        System.out.println("HASHSET");
+//
+//        HashMap<String,Integer> h1 = new HashMap<>();
+//        h1.put("alice",10);
+//        h1.put("bob",20);
+//        h1.put("tom",30);
+//        h1.put("charlie",40);
+//        h1.put("alan",50);
+//        h1.remove("alice");
+//        h1.remove("tom");
+//        System.out.println(h1.get("alan"));
+//        System.out.println(h1.size());
+//        System.out.println(h1);
+
+
+
+//        // HASHSET
+//        System.out.println("HASHSET");
+//
+//        HashSet<Integer> h2 = new HashSet<>();
+//        h2.add(10);
+//        h2.add(20);
+//        h2.add(30);
+//        h2.add(40);
+//        h2.add(50);
+//        h2.remove(40);
+//        h2.remove(10);
+//        System.out.println(h2.contains(56));
+//        System.out.println(h2.size());
+//        System.out.println(h2.isEmpty());
+//        System.out.println(h2);
+//        h2.clear();
+//        System.out.println(h2);
+//
