c93ae1ddb5fa93e9b75c5b2590733ec9068dfc92
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-30 20:21:59 +0500

    fixi

--- src/collection_of_data.java
+++ src/collection_of_data.java
@@ -2,38 +2,7 @@
 // Define a class `StudentDatabase` that holds a list of students. Each student is represented as a dictionary
 // with their `name` and `age`. Provide methods to add a student, remove a student, and display all students.
 
-
-import java.util.*;
-class main{
-    public static void main(String[] args){
-        student_database s1 = new student_database("alice",23);
-        s1.add();
-        s1.display();
-        s1.add();
-        s1.remove();
-
-    }
-}
-class student_database{
-    String name;
-    Integer age;
-
-    HashMap<String,Integer> h1 = new HashMap<>();
-
-    student_database(String name , Integer age){
-        this.name = name;
-        this.age= age;
-    }
-    void add(){
-        h1.put(this.name,this.age);
-    }
-    void remove(){
-        h1.remove(this.name);
-    }
-    void display(){
-        System.out.println(h1);
-    }
-}
+// EXAMPLE CODE  ----------->>
 
 
 //import java.util.*;
@@ -73,4 +42,40 @@ class student_database{
 //        System.out.println(h2);
 //        h2.clear();
 //        System.out.println(h2);
-//
+
+
+
+/* 
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        student_database s1 = new student_database("alice",23);
+        s1.add();
+        s1.display();
+        s1.add();
+        s1.remove();
+
+    }
+}
+class student_database{
+    String name;
+    Integer age;
+
+    HashMap<String,Integer> h1 = new HashMap<>();
+
+    student_database(String name , Integer age){
+        this.name = name;
+        this.age= age;
+    }
+    void add(){
+        h1.put(this.name,this.age);
+    }
+    void remove(){
+        h1.remove(this.name);
+    }
+    void display(){
+        System.out.println(h1);
+    }
+}
+*/
+
