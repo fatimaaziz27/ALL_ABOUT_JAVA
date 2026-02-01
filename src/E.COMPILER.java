d34145a1bb1585e4650d79475f5247dfa9809bfd
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-01 23:46:13 +0500

    fixo

--- src/E.COMPILER.java
+++ src/E.COMPILER.java
@@ -3,16 +3,7 @@ class E_COMPILER{
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your percentage");
-        int percentage = sc.nextInt();
-        switch () {
-            case value:
-                
-                break;
-        
-            default:
-                break;
-        }
+        System.out.print("Enter a number: ");
 }
     
 }
