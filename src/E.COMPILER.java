27e7d3b8f31cb0dbb2e68724e80e5ad8c71bb4fb
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-01 23:50:00 +0500

    fixi

--- src/E.COMPILER.java
+++ src/E.COMPILER.java
@@ -4,6 +4,22 @@ class E_COMPILER{
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num == 0){
+            System.out.print("the number is zero");
+        }
+        else if (num <= 0){
+            System.out.print("the number is negative");
+        }
+        
+        else if (num >= 0){
+            System.out.print("the number is positive");
+        }
+        
+        else{
+            System.out.print("invalid input");
+        }
 }
     
 }
