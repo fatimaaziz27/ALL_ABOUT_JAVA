7a7051a60140b262561d18a4b4270d531188b46c
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-01 23:51:46 +0500

    fixo

--- src/E.COMPILER.java
+++ src/E.COMPILER.java
@@ -2,24 +2,22 @@ import java.util.*;
 class E_COMPILER{
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // int num = sc.nextInt();
 
-        if (num == 0){
-            System.out.print("the number is zero");
-        }
-        else if (num <= 0){
-            System.out.print("the number is negative");
-        }
+        // if (num == 0){
+        //     System.out.print("the number is zero");
+        // }
+        // else if (num <= 0){
+        //     System.out.print("the number is negative");
+        // }
         
-        else if (num >= 0){
-            System.out.print("the number is positive");
-        }
+        // else{
+        //     System.out.print("the number is positive");
+        // }
+
         
-        else{
-            System.out.print("invalid input");
-        }
 }
     
 }
