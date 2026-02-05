f810aad82aea6c0596e512292d56d1683f01ffa1
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-06 00:00:18 +0500

    Fixo

--- src/collectionofdata.java
+++ src/collectionofdata.java
@@ -1,25 +1,28 @@
-public class collectionofdata {
+
 
 // colllection of data ------>
 
-/*
-//import java.util.Arrays;
-//class collectionofdata{
-//    public static void main(String[]args){
-//    int[] num = new int[5];
-//    num[3]=10;
-//    num[0]=20;
-//    System.out.println(Arrays.toString(num));
-//    num = new int[9];
-//    System.out.println(Arrays.toString(num));
-//// with integer data type
-//    int[] num1 = {1,2,3,4,5,6,7};
-//    System.out.println(Arrays.toString(num1));
-//// with String data type
-//    String[] fruits = {"frf","tfdyfy","dfhgf"};
-//    System.out.println(Arrays.toString(fruits));
-//}
-//}
+
+//     import java.util.Arrays;
+//     class collectionofdata{
+//     public static void main(String[]args){
+//     int[] num = new int[5];
+//     num[3]=10;
+//     num[0]=20;
+//     System.out.println(Arrays.toString(num));
+//     num = new int[9];
+//     System.out.println(Arrays.toString(num));
+//     // with integer data type
+//     int[] num1 = {1,2,3,4,5,6,7};
+//     System.out.println(Arrays.toString(num1));
+//     // with String data type
+//     String[] fruits = {"frf","tfdyfy","dfhgf"};
+//     System.out.println(Arrays.toString(fruits));
+//     }
+// }
+
+
+
 
 import java.util.*;
 class collectionofdata {
@@ -48,100 +51,8 @@ class collectionofdata {
         Integer d = (num.get(1)/num.get(2));
         num.set(3,d);
         System.out.println(num);
-
-
-
-
-//        num.add(70);
-//        num.add(30);
-//        num.add(20);
-//        System.out.println(num);
-//
-//        ArrayList<Integer> numl = new ArrayList<>();
-//        System.out.println(numl);
-//        numl.add(10);
-//        numl.add(20);
-//        numl.add(30);
-//        numl.add(40);
-//        numl.add(50);
-//        System.out.println(numl);
-//        numl.add(2,100);
-//        System.out.println(numl);
-//        numl.remove(1);
-//        System.out.println(numl);
-//        numl.set(2,100);
-//        System.out.println(numl);
-//
-//        System.out.println("LinkedList example");
-//
-//        LinkedList<Integer> numll = new LinkedList<>();
-//        System.out.println(numll);
-//        numll.add(10);
-//        numll.add(20);
-//        numll.add(30);
-//        numll.add(40);
-//        numll.add(50);
-//        System.out.println(numll);
-//        numll.add(2,100);
-//        System.out.println(numll);
-//        numll.remove(1);
-//        System.out.println(numll);
-//        numll.set(2,100);
-//        System.out.println(numll);
-
     }
 }
-*/
-
-
-
-//import java.util.Arrays;
-//class collectionofdata{
-//    public static void main(String[]args){
-//    int[] num = new int[5];
-//    num[3]=10;
-//    num[0]=20;
-//    System.out.println(Arrays.toString(num));
-//    num = new int[9];
-//    System.out.println(Arrays.toString(num));
-//// with integer data type
-//    int[] num1 = {1,2,3,4,5,6,7};
-//    System.out.println(Arrays.toString(num1));
-//// with String data type
-//    String[] fruits = {"frf","tfdyfy","dfhgf"};
-//    System.out.println(Arrays.toString(fruits));
-//}
-//}
-
-
-
-// class collectionofdata {
-//     public static void main (String[]args) {
-//         ArrayList<Integer> num = new ArrayList<Integer>();
-//         num.add(1);
-//         num.add(2);
-//         num.add(3);
-//         num.add(4);
-//         num.add(5);
-//         num.add(6);
-//         System.out.println(num);
-
-//         Integer add = (num.get(3)+num.get(4));
-//         num.set(0,add);
-//         System.out.println(num);
-
-//         Integer b = (num.get(4)-num.get(5));
-//         num.set(1,b);
-//         System.out.println(num);
-
-//         Integer c = (num.get(0)*num.get(1));
-//         num.set(2,c);
-//         System.out.println(num);
-
-//         Integer d = (num.get(1)/num.get(2));
-//         num.set(3,d);
-//         System.out.println(num);
-
 
 
 
@@ -181,6 +92,12 @@ class collectionofdata {
 //        System.out.println(numll);
 //        numll.set(2,100);
 //        System.out.println(numll);
+
+
+
+
+
+
 
 
 
@@ -248,4 +165,3 @@ class collectionofdata {
 //    }
 //}
 
-}
