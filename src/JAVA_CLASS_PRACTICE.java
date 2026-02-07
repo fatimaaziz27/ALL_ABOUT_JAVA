41aff0dac997ce715cb77c29c10de5ba244a2865
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-07 22:56:49 +0500

    fixio

--- src/JAVA_CLASS_PRACTICE.java
+++ src/JAVA_CLASS_PRACTICE.java
@@ -140,25 +140,32 @@ class main{
 
 class main{
     public static void main(String[]args){
+        
         ArrayList<Integer> numbers = new ArrayList<>();
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of number list: ");
         Integer size = sc.nextInt();
+        
         for (int i=1; i<=size; i++){
             System.out.println("enter your "+i+" number: ");
             Integer n = sc.nextInt();
             numbers.add(n);
         }
         System.out.println(numbers);
+
         int sum_of_all=0;
         for (int i : numbers){
             sum_of_all+=i;
         }
+
         int max_num = 0;
         for (int i : numbers){
             max_num++;
         }
+        
         System.out.println(sum_of_all);
+        System.out.println(max_num);
     }
 }
 
