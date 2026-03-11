bb11b79856388fbcd15e3698ea0fff19ab1956fa
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-12 03:47:06 +0500

    fixi

---
+++ src/INHERITANCE.java
@@ -0,0 +1,9 @@
+public class INHERITANCE {
+
+//     Inheritance in Java
+// Inheritance allows one class to acquire properties of another class.
+    
+class Subclass extends Superclass {
+    }
+    
+}
