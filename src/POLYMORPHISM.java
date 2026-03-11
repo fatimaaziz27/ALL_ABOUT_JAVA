4b555ec2ff8bf1d23bba1e2ff0ee695bb89fcce5
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-12 03:40:36 +0500

    fix

--- src/POLYMORPHISM.java
+++ src/POLYMORPHISM.java
@@ -1,124 +1,99 @@
+import java.lang.reflect.Method;
+
 public class POLYMORPHISM {
-    //Topic: Polymorphism in Java
+//Topic: Polymorphism in Java
+
 //Type of Polymorphism:
+
+
 //1. Method Overloading
+// Method overloading means multiple methods with the same name but different parameters.
+
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+
 //2. Method Overriding
+// Method overriding occurs when a subclass provides a specific implementation of a method already defined in the parent class.
 
-// class Polymorphism {
-//     public static void main(String[] args) {
-//         MathOperations mathOps = new MathOperations();
-//////
-//////         // Using method overloading
-//         System.out.println("Sum of 2 and 3: " + mathOps.add(2, 3));
-//         System.out.println("Sum of 2, 3 and 4: " + mathOps.add(2, 3,4));
-//         System.out.println("Sum of 2 and 3.5: " + mathOps.add(2, 3.5));
-//         System.out.println("Sum of 2 and 3.5: " + mathOps.add(2.5, 3.5));
-////     Animal5 dog2 = new Animal5();
-////     dog2.sound();
-////     Dog5 dog1 = new Dog5();
-////     dog1.sound();
-//     }
-// }
-
-////Example of Method Overloading
-// class MathOperations {
-//     // Method to add two integers
-//     int add(int a, int b) {
-//         return a + b;
-//     }
-//     // Overloaded method to add three integers
-//     int add(int a, int b, int c) {
-//         return a + b + c;
-//     }
-//     // Overloaded method to add two double values
-//     double add(double a, double b) {
-//         return a + b;
-//     }
-//    // Overloaded method to add one double value & one int value
-//    double add(int a, double b) {
-//         return a + b;
-//    }
-// }
-////Example of Method Overriding
-// class Animal5 {
-//     void sound() {
-//         System.out.println("Animal makes a sound");
-//     }
-// }
-// class Dog5 extends Animal5 {
-////     @Override
-//         void sound() {
-//             System.out.println("Dog barks");
-//         }
-//     }
-    
-
-// Create a program with:
-// A base class Account having private attribute accountNumber and
-// protected attribute balance, and methods deposit(), withdraw(), and
-// getBalance().
-// A subclass SavingsAccount that extends Account
-// and adds an attribute interestRate, and a method calculateInterest().
-// Override the withdraw() method in SavingsAccount to prevent
-// withdrawals that would reduce the balance below a minimum threshold.
-
-//Use scanner class for input from user wherever necessary. End the program when user choose to end it.
-
-// Create a program with:
-// A base class Account having private attribute accountNumber and
-// protected attribute balance, and methods deposit(), withdraw(), and
-// getBalance().
-// A subclass SavingsAccount that extends Account
-// and adds an attribute interestRate, and a method calculateInterest().
-// Override the withdraw() method in SavingsAccount to prevent
-// withdrawals that would reduce the balance below a minimum threshold.
-
-//Use scanner class for input from user wherever necessary. End the program when user choose to end it.
-
-
-class Accounts{
-    private String Accountnum;
-    protected double balance;
-    Accounts(String Account_num, double balance){
-        this.Accountnum = Account_num;
-        this.balance = balance;
-    }
-    void deposit(double amount){
-        this.balance += amount;
-        //this.balance = this.balance + amount
-    }
-    void withdraw(double amount){
-        if (balance == 0 || amount>this.balance) {
-            System.out.println("Can not withdraw");
-        }
-        else{
-            this.balance -= amount;
-            System.out.println("Withdrew: " + amount);
-            //this.balance = this.balance - amount
-        }
-    }
-    double getBalance(){
-        return this.balance;
-    }
-}
-class SavingAccounts extends Accounts{
-    private double interestRate;
-
-    SavingAccounts(String Account_num, double balance, double interestRate) {
-        super(Account_num, balance);
-        this.interestRate = interestRate;
-    }
-    @Override
-    void withdraw(double amount) {
-        if (balance - amount < 1000) {
-            System.out.println("Withdrawal denied: Balance cannot go below 1000");
-        } else {
-            super.withdraw(amount);
-        }
-    }
-    double calculateInterest() {
-        return balance * (interestRate / 100);
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
     }
 }
 
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog bark");
+    }
 }
+
+
+// Polymorphism Example ----->
+
+    class Polymorphism {
+        public static void main(String[] args) {
+            MathOperations m1 = new MathOperations();
+
+            // Using method overloading
+
+            System.out.println("Sum of 2 and 3: " + mathOps.add(2, 3));
+            System.out.println("Sum of 2, 3 and 4: " + mathOps.add(2, 3,4));
+            System.out.println("Sum of 2 and 3.5: " + mathOps.add(2, 3.5));
+            System.out.println("Sum of 2 and 3.5: " + mathOps.add(2.5, 3.5));
+
+        }
+    }
+
+    //Example of Method Overloading
+
+    class MathOperations {
+
+        // Method to add two integers
+        int add(int a, int b) {
+            return a + b;
+        }
+
+        // Overloaded method to add three integers
+        int add(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        // Overloaded method to add two double values
+        double add(double a, double b) {
+            return a + b;
+        }
+
+        // Overloaded method to add one double value & one int value
+        double add(int a, double b) {
+            return a + b;
+
+        }
+    }
+
+    //Example of Method Overriding
+
+    class Animal5 {
+
+        void sound() {
+            System.out.println("Animal makes a sound");
+        }
+    }
+
+    class Dog5 extends Animal5 {
+
+        //     @Override
+            void sound() {
+                System.out.println("Dog barks");
+
+            }
+        }
+    }
