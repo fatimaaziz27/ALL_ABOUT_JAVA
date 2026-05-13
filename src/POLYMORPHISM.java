c8cf8e43fde7e83a4b18d8c1b92ee5e249d59da9
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-13 12:25:27 +0500

    Update POLYMORPHISM.java

--- src/POLYMORPHISM.java
+++ src/POLYMORPHISM.java
@@ -1,8 +1,19 @@
 import java.lang.reflect.Method;
 
-//                      Polymorphism in Java
-//     The ability of different classes to be treated as instances of the same class through a common interface.
-//     It allows methods to do different things based on the object that it is acting upon, even though they share the same name.
+//=====================================================
+//                POLYMORPHISM IN JAVA
+//=====================================================
+
+// Definition:
+// The ability of different classes to be treated as
+// instances of the same class through a common interface.
+// It allows methods to perform different actions
+// depending on the object.
+
+//=====================================================
+//                 FLOW DIAGRAM
+//=====================================================
+
 
                     POLYMORPHISM
                            |
@@ -12,156 +23,157 @@ import java.lang.reflect.Method;
         -----------------------------------------
         |                                       |
         v                                       v
+
  Different Objects                     Different Behaviors
  Use the Same Method                   Based on the Object
-                  
-//       Type of Polymorphism:
-//      Method Overloading 
-//      Method Overriding
+
+
+//=====================================================
+//             TYPES OF POLYMORPHISM
+//=====================================================
+
 
                    POLYMORPHISM
                         |
         -----------------------------------
         |                                 |
         v                                 v
+
  Method Overloading              Method Overriding
-        |                                 |
- Same method name                Same method name
- Different parameters            Different implementation
-                    
+ (Compile-Time)                    (Run-Time)
 
-//1. Method Overloading
-// Method overloading means multiple methods with the same name but different parameters.
-// Flow Diagram:
-                  main()
-                     |
-                     v
-      Create MathOperations object
-                     |
-        --------------------------------
-        |              |              |
-        v              v              v
-
-     add(2,3)      add(2,3,4)    add(2,3.5)
-
-        |              |              |
-        v              v              v
-
-  add(int,int)   add(int,int,int) add(int,double)
-
-                      
-// public class POLYMORPHISM {
-// class Calculator {
-
-//     int add(int a, int b) {
-//         return a + b;
-//     }
-
-//     int add(int a, int b, int c) {
-//         return a + b + c;
-//     }
-// }
-
-                      
-                    
-//2. Method Overriding
-// Method overriding occurs when a subclass provides a specific implementation of a method already defined in the parent class.
-// Flow Diagram:
-                 main()
-                    |
-                    v
-           Create Dog5 object
-                    |
-                    v
-              dog.sound()
-                    |
-                    v
-          Overridden method runs
-                    |
-                    v
-               "Dog barks"
-                      
-// class Animal {
-//     void sound() {
-//         System.out.println("Animal sound");
-//     }
-// }
-
-// class Dog extends Animal {
-//     void sound() {
-//         System.out.println("Dog bark");
-//     }
-// }
+ Same Method Name                Same Method Name
+ Different Parameters            Different Implementation
 
 
+//=====================================================
+//               1. METHOD OVERLOADING
+//=====================================================
+
+// Method overloading means multiple methods
+// with the same name but different parameters.
 
 
+                  FLOW DIAGRAM
+
+                       main()
+                          |
+                          v
+          Create MathOperations Object
+                          |
+          -----------------------------------
+          |                 |               |
+          v                 v               v
+
+       add(2,3)        add(2,3,4)     add(2,3.5)
+
+          |                 |               |
+          v                 v               v
+
+    add(int,int)   add(int,int,int)  add(int,double)
+
+          |                 |               |
+          v                 v               v
+
+           5                 9              5.5
 
 
+class MathOperations {
 
-
-
-
-
-    
-//     Example Code:                  
-//     Flow Daigram ---->
-
-    class Polymorphism {
-        public static void main(String[] args) {
-            MathOperations m1 = new MathOperations();
-
-            // Using method overloading
-
-            System.out.println("Sum of 2 and 3: " + mathOps.add(2, 3));
-            System.out.println("Sum of 2, 3 and 4: " + mathOps.add(2, 3,4));
-            System.out.println("Sum of 2 and 3.5: " + mathOps.add(2, 3.5));
-            System.out.println("Sum of 2 and 3.5: " + mathOps.add(2.5, 3.5));
-
-        }
+    // Method to add two integers
+    int add(int a, int b) {
+        return a + b;
     }
 
-    //Example of Method Overloading
-
-    class MathOperations {
-
-        // Method to add two integers
-        int add(int a, int b) {
-            return a + b;
-        }
-
-        // Overloaded method to add three integers
-        int add(int a, int b, int c) {
-            return a + b + c;
-        }
-
-        // Overloaded method to add two double values
-        double add(double a, double b) {
-            return a + b;
-        }
-
-        // Overloaded method to add one double value & one int value
-        double add(int a, double b) {
-            return a + b;
-
-        }
+    // Overloaded method to add three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
     }
 
-    //Example of Method Overriding
-
-    class Animal5 {
-
-        void sound() {
-            System.out.println("Animal makes a sound");
-        }
+    // Overloaded method to add two double values
+    double add(double a, double b) {
+        return a + b;
     }
 
-    class Dog5 extends Animal5 {
-
-        //     @Override
-            void sound() {
-                System.out.println("Dog barks");
-
-            }
-        }
+    // Overloaded method to add one int and one double
+    double add(int a, double b) {
+        return a + b;
     }
+}
+
+//=====================================================
+//               2. METHOD OVERRIDING
+//=====================================================
+
+// Method overriding occurs when a subclass
+// provides its own implementation of a method
+// already defined in the parent class.
+
+
+                  FLOW DIAGRAM
+
+                        main()
+                           |
+                           v
+                  Create Dog Object
+                           |
+                           v
+                      dog.sound()
+                           |
+                           v
+               Overridden Method Called
+                           |
+                           v
+                     "Dog barks"
+
+
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    // Overriding parent method
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+//=====================================================
+//                    MAIN CLASS
+//=====================================================
+
+public class Polymorphism {
+
+    public static void main(String[] args) {
+
+        //=================================================
+        //           METHOD OVERLOADING
+        //=================================================
+
+        MathOperations mathOps = new MathOperations();
+
+        System.out.println("Sum of 2 and 3: "
+                + mathOps.add(2, 3));
+
+        System.out.println("Sum of 2, 3 and 4: "
+                + mathOps.add(2, 3, 4));
+
+        System.out.println("Sum of 2 and 3.5: "
+                + mathOps.add(2, 3.5));
+
+        System.out.println("Sum of 2.5 and 3.5: "
+                + mathOps.add(2.5, 3.5));
+
+        //=================================================
+        //           METHOD OVERRIDING
+        //=================================================
+
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
