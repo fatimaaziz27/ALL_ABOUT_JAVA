b30dd8174a2a9a68e14e21c57128b32241334662
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-17 15:28:46 +0500

    Refactor JAVA_OPPs.java with encapsulation examples

--- src/JAVA_OPPs.java
+++ src/JAVA_OPPs.java
@@ -86,6 +86,32 @@ class Car {
 
 */
 
+class Student {
+
+    // Private fields
+    private String name;
+    private int age;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
 
 
 
