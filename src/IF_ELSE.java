6507ba915ff30d90eefa24e91cb97cb475ceade2
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 20:33:33 +0500

    Update IF_ELSE.java

--- src/IF_ELSE.java
+++ src/IF_ELSE.java
@@ -81,7 +81,22 @@ class mainn {
 }
     
 
+class mainn {
+    public static void main(String[] args) {
 
+        int a = 10;
+        int b = 20;
+
+        if (a > b)
+            System.out.println("A is greater");
+        else if (b > a)
+            System.out.println("B is greater");
+        else
+            System.out.println("Both are equal");
+    }
+}
+
+    
 // from GPT
 import java.util.*;
 class mainn {
