2242eadcfbcaae5b4ff23b0bb087b02ce00cf138
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 20:32:57 +0500

    Update IF_ELSE.java

--- src/IF_ELSE.java
+++ src/IF_ELSE.java
@@ -59,7 +59,19 @@ class mainn {
 }
 
 
+class mainn {
+    public static void main(String[] args) {
 
+        int n = -5;
+
+        if (n > 0)
+            System.out.println("Positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
     
 
 
