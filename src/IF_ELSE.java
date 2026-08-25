5c5efd343e373ea21698803c3cbaded7c5a34478
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 20:31:16 +0500

    Update IF_ELSE.java

--- src/IF_ELSE.java
+++ src/IF_ELSE.java
@@ -55,6 +55,20 @@ class mainn {
 
 
 
+class mainn {
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
+
+
+
 // from GPT
 import java.util.*;
 class mainn {
